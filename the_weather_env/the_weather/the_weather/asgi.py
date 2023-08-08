93ae20047bb941aa433b4ec2cb672cd7cc6234f6
ERGYS HILA <ergysmania@gmail.com>
2023-08-08 00:45:05 -0400

    Update asgi.py

--- the_weather_env/the_weather/the_weather/asgi.py
+++ the_weather_env/the_weather/the_weather/asgi.py
@@ -3,7 +3,7 @@ ASGI config for the_weather project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this file,  see
 https://docs.djangoprojec t.com/en/4.2/howto/deployment/asgi/
 """
 
